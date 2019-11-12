a5ded6b1bc16b2818d88088dccbcf01889945f76
Venky-8 <2017bcs098@sggs.ac.in>
2019-11-12 12:05:25 +0530

    Cart page completed

--- cart/index.php
+++ cart/index.php
@@ -1,3 +1,9 @@
+<script src="../assets/js/jquery.min.js"></script>
+<script>
+// $(window).load(function(){
+// 	$('html,body').animate({scrollTop: $('#<?php //echo $_GET['target']; ?>').offset().top - 100 }, 'slow');
+// });
+</script>
 <?php
 session_start();
 require_once("dbcontroller.php");
@@ -5,10 +11,13 @@ $db_handle = new DBController();
 if(!empty($_GET["action"])) {
 switch($_GET["action"]) {
 	case "add":
-		if(!empty($_POST["quantity"])) {
-			$productByCode = $db_handle->runQuery("SELECT * FROM tblproduct WHERE code='" . $_GET["code"] . "'");
-			$itemArray = array($productByCode[0]["code"]=>array('name'=>$productByCode[0]["name"], 'code'=>$productByCode[0]["code"], 'quantity'=>$_POST["quantity"], 'price'=>$productByCode[0]["price"], 'image'=>$productByCode[0]["image"]));
-
+		if(!empty($_POST["quantity"]) || !empty($_POST["quantity_half"])) {
+			$productByCode = $db_handle->runQuery("SELECT * FROM menu WHERE code='" . $_GET["code"] . "'");
+			if(!empty($_POST["quantity_half"])){
+				$itemArray = array($productByCode[0]["code"]=>array('name'=>$productByCode[0]["item"], 'code'=>$productByCode[0]["code"], 'quantity'=>$_POST["quantity"], 'quantity_half'=>$_POST["quantity_half"], 'half'=>$productByCode[0]["half"], 'full'=>$productByCode[0]["full"]));
+			} else {
+				$itemArray = array($productByCode[0]["code"]=>array('name'=>$productByCode[0]["item"], 'code'=>$productByCode[0]["code"], 'quantity'=>$_POST["quantity"], 'full'=>$productByCode[0]["full"]));
+			}
 			if(!empty($_SESSION["cart_item"])) {
 				if(in_array($productByCode[0]["code"],array_keys($_SESSION["cart_item"]))) {
 					foreach($_SESSION["cart_item"] as $k => $v) {
@@ -43,13 +52,13 @@ switch($_GET["action"]) {
 }
 }
 ?>
-<HTML>
-<HEAD>
-<TITLE>Simple PHP Shopping Cart</TITLE>
+<html>
+<head>
+<title>Simple PHP Shopping Cart</title>
 <link href="style.css" type="text/css" rel="stylesheet" />
 <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
-</HEAD>
-<BODY>
+</head>
+<body>
 <div class="container-fluid">
 		<a href="../index.php" class="btn btn-outline-success float-right mt-2 ml-2"> Go Home</a>
 	<a href="../canteen.php" class="btn btn-outline-success float-right mt-2 ml-2"> Shop more </a>
@@ -76,26 +85,47 @@ if(isset($_SESSION["cart_item"])){
 </tr>
 <?php
     foreach ($_SESSION["cart_item"] as $item){
-        $item_price = $item["quantity"]*$item["price"];
+		$item_price = 0;
+		if(array_key_exists('quantity_half',$item)){
+			$item_price += $item['quantity_half']*$item['half'];
+			$item_price += $item['quantity']*$item['full'];
+		} else {
+			$item_price = $item["quantity"]*$item["full"];
+		}
 		?>
 				<tr>
-				<td><img src="<?php echo $item["image"]; ?>" class="cart-item-image" /><?php echo $item["name"]; ?></td>
+				<td><?php echo $item["name"]; ?></td>
 				<td><?php echo $item["code"]; ?></td>
-				<td style="text-align:right;"><?php echo $item["quantity"]; ?></td>
-				<td  style="text-align:right;"><?php echo "$ ".$item["price"]; ?></td>
-				<td  style="text-align:right;"><?php echo "$ ". number_format($item_price,2); ?></td>
+				<?php if(isset($item['quantity_half'])) { ?>
+					<td style="text-align:right;"><?php echo $item["quantity"] + $item["quantity_half"]; ?></td>
+				<?php } else { ?>
+					<td style="text-align:right;"><?php echo $item["quantity"]; ?></td>
+				<?php } ?>
+				<?php if(array_key_exists('half', $item)): ?>
+					<td  style="text-align:right;"><?php echo "₹ ". $item["half"] . "/" . $item["full"]; ?></td>
+				<?php else: ?>
+					<td  style="text-align:right;"><?php echo "₹ ".$item["full"]; ?></td>
+				<? endif ?>
+				<td  style="text-align:right;"><?php echo "₹". number_format($item_price,2); ?></td>
 				<td style="text-align:center;"><a href="index.php?action=remove&code=<?php echo $item["code"]; ?>" class="btnRemoveAction"><img src="icon-delete.png" alt="Remove Item" /></a></td>
 				</tr>
 				<?php
-				$total_quantity += $item["quantity"];
-				$total_price += ($item["price"]*$item["quantity"]);
+				if(array_key_exists('quantity_half',$item)){
+					$total_quantity += $item["quantity"];
+					$total_quantity += $item["quantity_half"];
+					$total_price += ($item["full"]*$item["quantity"]);
+					$total_price += ($item["half"]*$item["quantity_half"]);
+				} else {
+					$total_quantity += $item["quantity"];
+					$total_price += ($item["full"]*$item["quantity"]);
+				}
 		}
 		?>
 
 <tr>
 <td colspan="2" align="right">Total:</td>
 <td align="right"><?php echo $total_quantity; ?></td>
-<td align="right" colspan="2"><strong><?php echo "$ ".number_format($total_price, 2); ?></strong></td>
+<td align="right" colspan="2"><strong><?php echo "₹ ".number_format($total_price, 2); ?></strong></td>
 <td></td>
 </tr>
 </tbody>
@@ -107,29 +137,54 @@ if(isset($_SESSION["cart_item"])){
 <?php
 }
 ?>
+
+<a id="checkout" class="btn btn-outline-success float-right my-auto text-center disabled" href="order.php">Checkout</a>
+
 </div>
 
 <div id="product-grid">
 	<div class="txt-heading">Products</div>
+	<div class="row">
 	<?php
-	$product_array = $db_handle->runQuery("SELECT * FROM tblproduct ORDER BY id ASC");
+	$product_array = $db_handle->runQuery("SELECT * FROM menu ORDER BY id ASC");
 	if (!empty($product_array)) {
 		foreach($product_array as $key=>$value){
 	?>
-		<div class="product-item">
+	<div class="col-3 mb-3">
+		<div class="product-item" id="<?php echo $product_array[$key]["code"] ?>">
 			<form method="post" action="index.php?action=add&code=<?php echo $product_array[$key]["code"]; ?>">
-			<div class="product-image"><img src="<?php echo $product_array[$key]["image"]; ?>"></div>
-			<div class="product-tile-footer">
-			<div class="product-title"><?php echo $product_array[$key]["name"]; ?></div>
-			<div class="product-price"><?php echo "$".$product_array[$key]["price"]; ?></div>
-			<div class="cart-action"><input type="text" class="product-quantity" name="quantity" value="1" size="2" /><input type="submit" value="Add to Cart" class="btnAddAction" /></div>
+			<div class="product-tile-footer card h-100" style="width: 20rem;">
+			<div class="d-flex flex-column">
+				<div class="product-title"><?php echo $product_array[$key]["item"]; ?></div>
+			<?php if(!empty($product_array[$key]['half'])) { ?>
+				<div class="product-price"><span>Half: <?php echo "₹".$product_array[$key]["half"]; ?><input type="text" class="product-quantity" name="quantity_half" value="0" size="2" /></span></div>
+				<div class="product-price"><span>Full: <?php echo "₹".$product_array[$key]["full"]; ?><input type="text" class="product-quantity" name="quantity" value="1" size="2" /></span></div>
+				<div class="cart-action"><input type="submit" value="Add to Cart" class="btnAddAction" /></div>
+				</div>
+			<?php } else { ?>
+				<div class="product-price"><?php echo "₹".$product_array[$key]["full"]; ?></div>
+				<div class="cart-action"><input type="text" class="product-quantity" name="quantity" value="1" size="2" /><input type="submit" value="Add to Cart" class="btnAddAction" /></div>
+				</div>
+			<?php } ?>
 			</div>
 			</form>
 		</div>
+	</div>
 	<?php
 		}
 	}
 	?>
 </div>
-</BODY>
-</HTML>
+</div>
+
+<script>
+	// document.getElementById('checkout').addEventListener('click', function() {
+	// 	if(this.classList.contains('disabled')) {
+	// 		this.classList.remove('disabled');
+	// 	}
+	// });
+	// checkout.classList.removeClass('disabled');
+</script>
+
+</body>
+</html>
